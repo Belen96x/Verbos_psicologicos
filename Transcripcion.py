e2f26d2911506c4a8dcf58930eee4407f8e83b26
Belen Saavedra <belen.saavedra.39@gmail.com>
2023-02-03 11:16:01 -0300

    Modificaciones del script por reunion de presentacion

--- Transcripcion.py
+++ Transcripcion.py
@@ -1,7 +1,7 @@
 from deepgram import Deepgram
 import asyncio, json
 
-DEEPGRAM_API_KEY = 'a57564e5cfb8c2a1b764b123668c3da91811f9a7'
+DEEPGRAM_API_KEY = 'a57564e5cfb8c2a1b764b123668c3da91811f9a7' #Campo modificable (!)
 
 PATH_TO_FILE = (r"C:\Users\belen\Desktop\Investigacion_NLP\Verbos_psicológicos\Audio_prueba.m4a")
 
@@ -11,8 +11,8 @@ async def main():
   # Abre el archivo
   with open(PATH_TO_FILE, 'rb') as audio:
     # Reemplazar el mimetype como sea necesario
-    source = {'buffer': audio, 'mimetype': 'audio/mp3'}
-    response = await dg_client.transcription.prerecorded(source, { 'language': 'es', 'punctuate': True })
+    source = {'buffer': audio, 'mimetype': 'audio/m4a'}
+    response = await dg_client.transcription.prerecorded(source, { 'language': 'es', 'punctuate': True, 'diarize': True })
     print(json.dumps(response, indent=4))
 
 asyncio.run(main())
